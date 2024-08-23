6a989ec54e524de06a1578e546115d2dec1e5a10
Dušan Čolić <dcolic3121rn@raf.rs>
2024-08-23 15:25:48 +0200

    login and sign up forms are finished

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -15,27 +15,70 @@ export class LoginComponent implements OnInit, OnDestroy {
   @ViewChild('container') container: ElementRef;
 
   loginFailed: boolean = false;
+  signUpForm!: FormGroup;
+  signUpFailed: boolean = false;
   showPassword: boolean = false;
   loginForm!: FormGroup;
+  errorMessages = {
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'email', message: 'Enter a valid email' }
+    ],
+    password: [
+      { type: 'required', message: 'Password is required' },
+      { type: 'minlength', message: 'Password must be at least 8 characters long' }
+    ],
+    firstname: [
+      { type: 'required', message: 'First name is required' },
+      { type: 'minlength', message: 'First name must be at least 2 characters long' }
+    ],
+    lastname: [
+      { type: 'required', message: 'Last name is required' },
+      { type: 'minlength', message: 'Last name must be at least 2 characters long' }
+    ]
+  };
+  errorMessage!: string;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['',Validators.email],
-      password: ['',[Validators.required,Validators.required]]
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(8)]]
+    });
+
+    this.signUpForm = this.formBuilder.group({
+      firstname: ['',[Validators.required,Validators.minLength(2)]],
+      lastname: ['',[Validators.required,Validators.minLength(2)]], 
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(8)]]
     });
 
     this.loginFailed = false;
+    this.signUpFailed = false;
     this.showPassword = false;
   }
 
   onLoginSubmit() : void {
     console.log(this.loginForm.value);
     if (this.loginForm.valid) {
+      const loginEmail = this.loginForm.get('email')?.value;
+      const loginPassword = this.loginForm.get('password')?.value;
+      
+
       this.loginFailed = false;
     } else {
       this.loginFailed = true;
+    }
+  }
+
+  onSignUpSubmit() : void {
+    console.log(this.signUpForm.value);
+    console.log(this.signUpForm.valid);
+    if (this.signUpForm.valid) {
+      this.signUpFailed = false;
+    } else {
+      this.signUpFailed = true;
     }
   }
 
